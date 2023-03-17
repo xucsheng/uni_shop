bbfac33fa5b29408c48aec639fba4ab495f1a79e
xuchunsheng <xuchunsheng123>
2023-03-17 16:40:38 +0800

    分类查询页面功能开发,xu_csheng 20230317

--- pages/tabBar/cate/cate.vue
+++ pages/tabBar/cate/cate.vue
@@ -1,6 +1,30 @@
 <template>
 	<view>
-		分类
+		<view class="scroll-view-container">
+			<!--左侧滑动区域-->
+			<scroll-view class="left-scroll-view"   scroll-y :style="{height: wh + 'px'}">
+				<block v-for="(item,index) in cateList" :key="index">
+				   <view   :class="['left-scroll-view-item', index === active ? 'active' :'']"  @click="activeChanged(index)">{{item.cat_name}}</view>
+				</block>
+			</scroll-view>
+			<!--右侧滑动区域-->
+			<scroll-view class="right-scroll-view"  scroll-y :style="{height: wh + 'px'}">
+				<view class="cate-lv2"  v-for="(item2,index) in cateLevel2" :key="index">
+					<!--二级分类标题-->
+					<view class="cate-lv2-title">/{{item2.cat_name}}/</view>
+					<!--动态渲染三级分类的类别数据-->
+					<view class="cate-lv3-list">
+						<!--三级分类的item项-->
+						<view class="cate-lv3-item" v-for="(cateLevel3,i) in item2.children" :key="i">
+							<!--三级分类的图片-->
+							<image :src="cateLevel3.cat_icon.replace('dev','web')"></image>
+							<!--三级分类的文本-->
+							<text>{{cateLevel3.cat_name}}</text>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +32,101 @@
 	export default {
 		data() {
 			return {
-				
+				// 当前设备可使用的高度
+				wh:0,
+				// 分类数据
+				cateList:[],
+				active:0,
+				// 二级分类列表
+			    cateLevel2: [],
 			}
 		},
-		methods: {
+		onLoad(){
+			// 获取当前系统的信息
+			const sysInfo = uni.getSystemInfoSync();
+			//console.log(sysInfo);
+			// 为 wh 窗口可用高度动态赋值
+			this.wh = sysInfo.windowHeight;
+			this.getCateList();
 			
+		},
+		methods: {
+			async getCateList(){
+				const {data:res}  = await uni.$http.get('/api/public/v1/categories');
+				if(res.meta.status !=200){
+					return uni.$showMsg();
+				}
+				this.cateList = res.message;
+				// 为二级分类赋值
+				this.cateLevel2 = res.message[0].children
+			},
+			activeChanged(index){
+				this.active = index;
+				// 重新为二级分类赋值
+				this.cateLevel2 = this.cateList[index].children;
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+.scroll-view-container {
+  display: flex;
 
+  .left-scroll-view {
+    width: 120px;
+
+    .left-scroll-view-item {
+      line-height: 60px;
+      background-color: #f7f7f7;
+      text-align: center;
+      font-size: 12px;
+
+      // 激活项的样式
+      &.active {
+        background-color: #ffffff;
+        position: relative;
+
+        // 渲染激活项左侧的红色指示边线
+        &::before {
+          content: ' ';
+          display: block;
+          width: 3px;
+          height: 30px;
+          background-color: #c00000;
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+}
+.cate-lv2-title{
+	font-size: 12px;
+	font-weight: bold;
+	text-align: center;
+	padding: 15px 0;
+}
+.cate-lv3-list {
+  display: flex;
+  flex-wrap: wrap;
+  
+   .cate-lv3-item{
+	width: 33.33%;
+	margin-bottom: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-bottom: 10px;
+	image{
+	  width: 60px;
+	  height: 60px;
+	}
+	text{
+	  font-size: 12px;
+	 }
+   }
+}
 </style>
